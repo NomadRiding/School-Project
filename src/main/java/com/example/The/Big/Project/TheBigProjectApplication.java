cdd2d0e83b493d29af0579e024af72cb4539c271
nomadriding <fundora.a@gmail.com>
2024-07-04 11:10:17 -0400

    initial commit

---
+++ src/main/java/com/example/The/Big/Project/TheBigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.The.Big.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBigProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/The/Big/Project/model/User.java
@@ -0,0 +1,8 @@
+package com.example.The.Big.Project.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
