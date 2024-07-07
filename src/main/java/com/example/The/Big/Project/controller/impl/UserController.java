1e5819d8e345f31d357cd4879b27b074fcb30ebd
nomadriding <fundora.a@gmail.com>
2024-07-07 14:27:44 -0400

    connect repository to model for user

--- src/main/java/com/example/The/Big/Project/controller/impl/UserController.java
+++ src/main/java/com/example/The/Big/Project/controller/impl/UserController.java
@@ -1,4 +1,13 @@
 package com.example.The.Big.Project.controller.impl;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @RequestMapping("/api")
+    public String getGreeting(){
+        return "Hello 102Foxtrot!";
+    }
 }
--- src/main/java/com/example/The/Big/Project/model/User.java
+++ src/main/java/com/example/The/Big/Project/model/User.java
@@ -1,8 +1,66 @@
 package com.example.The.Big.Project.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import  lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String username;
     private String password;
     private String email;
+
+    //empty constructor as required
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
