b2c8008e5d199df7ee00393c9d6da30e318fa26a
nomadriding <fundora.a@gmail.com>
2024-07-08 14:02:57 -0400

    create author repository instance

---
+++ src/main/java/com/example/The/Big/Project/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.example.The.Big.Project.repository;
+
+
+import com.example.The.Big.Project.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
