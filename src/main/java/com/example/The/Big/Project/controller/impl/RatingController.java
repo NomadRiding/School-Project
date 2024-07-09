3085e842b28fd8f60b1498579b22f07df60ac3c5
nomadriding <fundora.a@gmail.com>
2024-07-09 07:29:04 -0400

    add mapping

--- src/main/java/com/example/The/Big/Project/controller/impl/RatingController.java
+++ src/main/java/com/example/The/Big/Project/controller/impl/RatingController.java
@@ -1,4 +1,10 @@
 package com.example.The.Big.Project.controller.impl;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class RatingController {
 }
