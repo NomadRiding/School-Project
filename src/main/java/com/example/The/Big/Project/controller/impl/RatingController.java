613ba6a1ffff95dd565747c7feac6e721e7713bd
nomadriding <fundora.a@gmail.com>
2024-07-09 07:35:15 -0400

    create findAll for rating

--- src/main/java/com/example/The/Big/Project/controller/impl/RatingController.java
+++ src/main/java/com/example/The/Big/Project/controller/impl/RatingController.java
@@ -1,10 +1,21 @@
 package com.example.The.Big.Project.controller.impl;
 
 
+import com.example.The.Big.Project.model.Rating;
+import com.example.The.Big.Project.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class RatingController {
+@Autowired
+    RatingRepository ratingRepository;
+
+@GetMapping("/ratings")
+    public List<Rating> getAllRatings(){return ratingRepository.findAll();}
 }
--- src/main/java/com/example/The/Big/Project/repository/RatingRepository.java
+++ src/main/java/com/example/The/Big/Project/repository/RatingRepository.java
@@ -1,4 +1,9 @@
 package com.example.The.Big.Project.repository;
 
+import com.example.The.Big.Project.model.Rating;
+
+import java.util.List;
+
 public interface RatingRepository {
+    List<Rating> findAll();
 }
